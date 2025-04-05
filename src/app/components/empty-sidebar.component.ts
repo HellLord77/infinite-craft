7476674558e7b0e41854ee71c204aec362dd2451
HellLord <ratul.debnath.year@gmail.com>
2025-04-06 01:18:48 +0600

    fix some layout problems

--- src/app/components/empty-sidebar.component.ts
+++ src/app/components/empty-sidebar.component.ts
@@ -1,11 +1,9 @@
 import {Component} from '@angular/core';
 
-import {EmptySidebarIconComponent} from './empty-sidebar-icon.component';
-
 @Component({
   selector: 'app-empty-sidebar',
   standalone: true,
-  imports: [EmptySidebarIconComponent],
+  imports: [],
   templateUrl: './empty-sidebar.component.html',
   styleUrl: './empty-sidebar.component.css',
 })
